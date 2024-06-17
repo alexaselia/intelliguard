b06700db10a63d6f4d1846154d585e5c4da394af
Odair <odairalexandreneto@gmail.com>
2024-06-17 13:57:10 -0300

    Change to the proxy URL

--- src/components/MapboxMap.tsx
+++ src/components/MapboxMap.tsx
@@ -106,7 +106,7 @@ const MapboxMap: React.FC<MapboxMapProps> = ({ cameras }) => {
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: 'https://159.203.23.205:3001/mapbox-style',
+      style: 'https://vms.megaguardiao.com.br:3001/mapbox-style',
       center: initialCenter,
       zoom: 3,
       pitch: 45,
