643e6302a5f51f407098647285730e7355365795
Odair <odairalexandreneto@gmail.com>
2024-06-18 15:39:23 -0300

    Prerender changes to reset password page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
