dd35f94e8093fc9b256efffdbe6df984c239b17f
Odair <odairalexandreneto@gmail.com>
2024-06-18 15:48:38 -0300

    Redefining mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client"; // Add this line
+"use client";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
